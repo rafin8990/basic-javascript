11402fe7aa8ba87bc0378adfdab239e16e75d867
Rafin Hossain <rafin8990@gmail.com>
2022-08-08 22:36:04 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,457 @@
+// javascript  module startedd 
+
+/* 
+modiule 16 : practice problem 1
+
+var momGive=1000;
+var applePrice=400;
+var orangePrice=300;
+var totalFruitPrice=applePrice + orangePrice;
+var backTakaa= momGive - totalFruitPrice;
+console.log(backTakaa);
+ */
+
+
+/*  modiule 16: practicee problem 2
+
+var mathematicsMark=75.25;
+var biologyMark=65;
+var chemistryMark=80;
+var physicsMark=35.45;
+var banglaMark=99.25;
+var totalMarks= mathematicsMark + biologyMark + chemistryMark + physicsMark + banglaMark ;
+var avarage = totalMarks / 5 ;
+console.log(avarage) */
+
+
+/* module 16: practice problemm 3
+ var string1='I want to be';
+ var string2='a web developer';
+ var fullmeaning = string1 + ' ' + string2
+ console.log(fullmeaning) */
+ 
+
+/*  module 16: practice problem 4
+
+var givenNumber=119;
+var devidedBy=5;
+var output = givenNumber % devidedBy;
+console.log(output) */
+
+/* *************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************** */
+
+
+
+/* module 17 : practice problem 1
+var fruits=['apple','banana','orange'];
+var know = fruits.indexOf('banana');
+console.log(know);
+fruits[1]='mango';
+console.log(fruits);
+fruits.pop('orange');
+console.log(fruits);
+fruits.push('Watermelon');
+console.log(fruits) */
+
+/*  module 17: practicce problem  2:
+var myScore=85;
+if(myScore > 80){
+  console.log('Got A+');
+}
+
+  else if(myScore > 60){
+    console.log('Got B grade');
+  }
+
+  else if(myScore > 50){
+    console.log ('Got C grade');
+  }
+
+  else if(myScore > 40){
+    console.log('Got D grade');
+  }
+
+
+
+else{
+  console.log("fail");
+}
+
+var tomScore=66;
+if(tomScore > 80){
+  console.log('Got A+');
+}
+  else if(tomScore > 60){
+    console.log('Got B grade');
+  }
+  else if(tomScore > 50){
+    console.log ('Got C grade');
+  }
+  else if(tomScore > 40){
+    console.log('Got D grade');
+  }
+
+else{
+  console.log("fail");
+}
+var janeScore=95;
+
+if(janeScore > 80){
+  console.log('Got A+');
+}
+  else if(janeScore > 60){
+    console.log('Got B grade');
+  }
+  else if(janeScore > 50){
+    console.log ('Got C grade');
+  }
+  else if(janeScore > 40){
+    console.log('Got D grade');
+  }
+else{
+  console.log("fail");
+}
+var peterScore=56;
+if(peterScore > 80){
+  console.log('Got A+');
+}
+  else if(peterScore> 60){
+    console.log('Got B grade');
+  }
+  else if(peterScore > 50){
+    console.log ('Got C grade');
+  }
+  else if(peterScore > 40){
+    console.log('Got D grade');
+  }
+else{
+  console.log("fail");
+}
+
+var johnScore=40;
+if(johnScore > 80){
+  console.log('Got A+');
+}
+  else if(johnScore> 60){
+    console.log('Got B grade');
+  }
+  else if(johnScore > 50){
+    console.log ('Got C grade');
+  }
+  else if(johnScore > 40){
+    console.log('Got D grade');
+  }
+
+else{
+  console.log("fail");
+}
+
+ */
+
+ /* module17 : practice problem 3
+
+var num1=13;
+var num2=79;
+var num3=45;
+if(num1>num2){
+  if(num1>num3){
+    console.log(num1);
+  }
+  else{
+    console.log(num3)
+  }
+}
+else{
+  if(num2>num3){
+    console.log(num2);
+  }
+  else{
+    console.log(num3)
+  }
+} 
+*/
+// condition problem solve:
+/* var myMoney = 'x';
+var applePrice= 80000;
+var gamingLaptopPrice=60000;
+var usedLaptopPrice=20000;
+
+if(myMoney> applePrice){
+  console.log('i will buy an iphone');
+}
+else if(myMoney> gamingLaptopPrice){
+  console.log('i will buy an gaming laptop');
+}
+else if(myMoney>usedLaptopPrice){
+  console.log('i will buy an used laptop')
+}
+else{
+  console.log( i will use only mobile phone);
+} */
+
+// loop problem1
+// Display 'ajke amar mon valo nei' for 30 times 
+// var contain=0;
+// while(contain < 30 ){
+//   console.log('ajke amr mon valo nei');
+//   contain++;
+// }
+
+// display numbers between 58 to 98
+/*  var numbers = 58;
+while(numbers<=98){
+  console.log(numbers);
+  numbers++; 
+} */
+// show all even numbers from 412 to 456 
+/* var numbers= 412;
+while(numbers<= 456){
+  console.log(numbers);
+  numbers+=2
+} */
+
+// show all odd numbers 581 to 623
+/* var numbers=581;
+while(numbers<623){
+  console.log(numbers);
+  numbers+=2;
+} */
+// declear an array for all the topic you have learned last few DynamicsCompressorNode.Display then as output 
+/* var topics =['variable', 'array', 'condition', 'loop', 'function', 'object'];
+for(var i=0; i<6; i++){
+  var topic= topics[i];
+  console.log(topic);
+
+} */
+
+// create an array for all the mobile phone .Display all the elements of the array by using a while loop 
+
+/* var mobileName =['Nokia', 'Samsung', 'Symphoni', 'microsoft', 'lenovo', 'Vivo', 'Oppo' ];
+var mobiles=0;
+while(mobiles<7){
+  var mobile=mobileName[mobiles];
+  mobiles++;
+  console.log(mobile);
+}
+ */
+
+// Run a loop from 30 to 86 . this loop will stop if the value get higher than 44
+/* for(var i=30; i<=86; i++ ){
+  if(i>44){
+    break;
+  }
+  console.log(i);
+} */
+
+
+// Write the price of the books that you have . Display the prices if the pricess lower than 200
+
+/* var bookPrice=[120, 200, 260, 150, 190, 300, 425, 180, 500];
+for(var i=0; i<bookPrice.length; i++){
+  var books=bookPrice[i];
+  if(books>200){
+continue;
+}
+console.log(books);
+} */
+
+
+
+// মডিউল ১৮ : প্রাকটিস প্রবলেম ১: প্রাকটিস চ্যালেঞ্জ-১
+
+// প্রতিদিন তোমার কাজ কি? 
+
+// ১) রাত ৮ টা বাজে মডিউল আনলক করো  
+
+// ২) ফটাফট ভিডিও দেখে দেখে প্রাকটিস করো
+
+// ৩) ভিডিও দেখতে দেখতে নোটস নাও 
+
+// ৪) মডিউল শেষ হলে পুরা মডিউল নিজে নিজে প্রাকটিস করো 
+
+// ৫) কোন কিছু বুঝতে না পারলে (চিন্তা করে দেখো এইখানে কিন্তু একটা শর্ত আছে ), সাপোর্ট সেশনে জয়েন করো 
+
+// এখন তোমার কাজ হচ্ছে একটা for লুপ ১০ বার চালিয়ে উপরের জিনিসগুলা আউটপুট হিসেবে দেখানো। 
+
+/* var myDailyWork =['unlock module', 'video dekhe practice', 'notes neo', 'full modiul practice', 'join support session'];
+
+for(var j=0; j<10; j++){
+  for(var i=0; i <myDailyWork.length; i++ ){
+    var work = myDailyWork[i];
+    console.log(work);
+  
+  }
+}
+ */
+
+//  modiul 21 problem solve
+
+// Jim get 84 Number, Dena get 99 number & Chinku get 77 number : 
+// teacher said that those who get higher marks will get the cake . Find out that who will get the cake . 
+
+// 1st way 
+// const jim=84;
+// const dena=99;
+// const chinku=77;
+// if(jim>dena){
+//   if(jim>chinku){
+//     console.log('jim will get the cake');
+//   }
+//   else{ 
+//     console.log('chinku will get the cake');
+//   }
+// }
+// else{
+//   if(dena>chinku){
+//     console.log('dena will get the cake');
+//   }
+//   else{
+//     console.log('Chinku will get the cake');
+//   }
+// }
+
+// 2nd way 
+/*   const jim=84;
+  const dena=99;
+  const chinku=77;
+  if( jim>dena && jim>chinku){
+    console.log('jim will get the cake');
+  }
+  else if ( dena> jim && dena >chinku){
+    console.log('dena will get the cake');
+  }
+  else{
+    console.log('chinku will get the cake');
+  } */
+
+
+
+
+// modiul 21: home work ; get the lowest value of an array function 
+/* function minInArray(numbers){
+  let smallest = numbers[0];
+  for(let i=0; i<numbers.length; i++){
+    const index=i;
+    const element=numbers[index];
+    if(element<smallest){
+      smallest=element
+    }
+  }
+  return smallest;
+
+}
+const value =[167, 190, 120, 165, 137, 265];
+const lowest = minInArray(value);
+console.log(lowest); */
+
+
+//  modiul 21: get the higher value of an array:  
+/* function maxInArray(numbers){
+  let largest = numbers[0];
+  for(let i=0; i<numbers.length; i++){
+    const index=i;
+    const element=numbers[index];
+    if(element>largest){
+      largest=element
+    }
+  }
+  return largest;
+
+}
+const value =[167, 190, 120, 165, 137, 265];
+const tallest = maxInArray(value);
+console.log(tallest); */
+
+// modiule21: problem
+/* 
+1.display 1-100 number 
+2.if the number divisible by 3 then replace it with 'raf'
+3.if the number divisible by 5 then replace it with 'fin'
+4.if the number divisible by 3 & 5 then replace it with 'rafin'
+
+*/
+/* 
+for(let i=1; i<=100 ; i++){
+  if(i%3 === 0 && i%5 === 0){
+    console.log('Rafin');
+  }
+  else if(i%3 === 0){
+    console.log('raf');
+  }
+  else if (i%5 === 0){
+    console.log('fin');
+  }
+  else{
+    console.log(i);
+  }
+} */
+
+
+// modiule 22: cheak total quantity
+
+/* function priceCalculator(penQuantity, bookQuantity, dairyQuantity){
+ const perPen=5;
+ const perBook=100;
+ const perDairy=60;
+
+ const totalPenPrice=perPen * penQuantity ;
+ const totalBookPrice= perBook * bookQuantity;
+ const totalDairyPrice= perDairy * dairyQuantity
+
+ const totalPrice= totalBookPrice + totalDairyPrice + totalPenPrice ;
+ return totalPrice;
+
+}
+const totalPrice=priceCalculator(10, 20, 30);
+console.log(totalPrice); */
+
+// modiule-22: get the heighest value of an arrray
+ 
+
+/* function getHighestNumber(numbers){
+  let highestNumber=[0];
+  for(let i=0 ; i < numbers.length; i++){
+    const number=numbers[i];
+    if(number > highestNumber){
+      highestNumber=number;
+    }
+
+  }
+  return highestNumber;
+}
+const numbers = [40, 20, 56, 60, 23, 34, 15, 35, 10,55, 68];
+const myHighest = getHighestNumber(numbers);
+console.log(myHighest); */
+
+// module 22 - practice video 8 
+// 1st 100--->100
+// 2nd 100---->90
+// rest----->80
+
+
+/* function ticketPrice(ticketQuantity){
+  const first100Price=100;
+  const second100Price=90;
+  const restTicketPrice=80;
+  if(ticketQuantity <= 100){
+    const price=ticketQuantity*first100Price;
+    return price;
+  }
+  else if(ticketQuantity<=200){
+    const first100rate=100*first100Price;
+    const restticketQuantity=ticketQuantity-100;
+    const restTicketrate=second100Price*restticketQuantity;
+    const totalRate=first100rate + restTicketrate
+    return totalRate;
+  }
+  else{
+  const  first100cost=100*first100Price;
+    const second100cost=100*second100Price;
+    restticketQuantity=ticketQuantity-200;
+    restTicketCost=restticketQuantity*restTicketPrice;
+    const totalCost=first100cost+second100cost+restTicketCost;
+    return totalCost;
+  }
+}
+const price=ticketPrice(300);
+console.log(price); */
